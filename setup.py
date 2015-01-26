081887500d91cb1a54d0a54f79b5d09d4a6e3d45
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-01-26 09:39:53 +0100

    Setup installation commands

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simcity_client',
+      version='0.1',
+      description='Python SIM-CITY client using PiCaS.',
+      author='Joris Borgdorff',
+      author_email='j.borgdorff@esciencecenter.nl',
+      url='https://esciencecenter.nl/projects/sim-city/',
+      packages=['simcity_client'],
+      install_requires=['couchdb']
+     )
