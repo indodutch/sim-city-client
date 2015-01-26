b15add00f435e165bab760925a19dd0019d0fd44
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-01-26 18:17:19 +0100

    Added example execution script

---
+++ scripts/example_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import json
+import os
+
+if __name__ == '__main__':
+    print("Input:")
+    print(sys.argv)
+    
+    tmp_dir = sys.argv[1]
+    in_dir = sys.argv[2]
+    out_dir = sys.argv[3]
+    
+    with open(os.path.join(in_dir, 'input.json')) as f:
+        params = json.load(f)
+    
+    output = float(params['a'])*51.0
+    print("Output:")
+    print(output)
+    
+    with open(os.path.join(out_dir, 'a_out')) as f:
+        print(output, file=f)
+        
+    print("Done")
