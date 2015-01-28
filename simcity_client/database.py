c7a6c1686886572953e402ca110430a2a4584565
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-01-28 17:16:53 +0100

    Updated documentation for database file

--- simcity_client/database.py
+++ simcity_client/database.py
@@ -1,15 +1,22 @@
 """
+Provides access to a CouchDB database.
+Originally used PiCaS, now all functionality directly accesses CouchDB.
+
 Created on Mon Jun  4 11:40:06 2012
+Updated Wed Jan 28 17:12 2015
 
 @author: Jan Bot
+@author: Joris Borgdorff
 """
-from couchdb.client import Server
 from simcity_client.token import Token
 import random
+import numpy as np
+
+# Couchdb imports
 import couchdb
 from couchdb.design import ViewDefinition
 from couchdb.http import ResourceConflict
-import numpy as np
+from couchdb.client import Server
         
 class CouchDB(object):
     """Client class to handle communication with the CouchDB back-end.
@@ -33,16 +40,21 @@ class CouchDB(object):
     
     def get_all_tokens(self, design_doc, view, **view_params):
         """
-        Get tokens from the specified view.
-        .. function:: get_all(viewLoc[, view_params={}])
-        :param viewLoc: location of the view in 'design/view' notation.
-        :param view_params: optional extra parameters for the view.
-        :return: row list returned by the view.
+        Get tokens from the specified view with token _id as key.
+        :param design_doc: design document of the view
+        :param view: name of the view, having the token _id as keys
+        :param view_params: name of the view optional extra parameters for the view.
+        :return: a list of Token objects in the view
         """
         view = self.view(design_doc, view, **view_params)
         return [self.get_token(row['key']) for row in view.rows]
     
     def get_token(self, id):
+        """
+        Get the token associated to the given ID
+        :param id: _id string of the token
+        :return: Token object with given id
+        """
         return Token(self.db[id])
     
     def get_single_token(self, design_doc, view, window_size=1, **view_params):
@@ -57,12 +69,31 @@ class CouchDB(object):
         return self.get_token(row['key'])
     
     def view(self, design_doc, view, **view_params):
+        """
+        Get the data from a view
+        
+        :param design_doc: name of the design document
+        :param view: name of the view
+        :return: iterator over the view; the rows property contains the rows.
+        """
         return self.db.view(design_doc + '/' + view, **view_params)
     
     def token_iterator(self, design_doc, view):
+        """ Iterate over all tokens in a view.
+        
+        Gets one token at a time
+        :param design_doc: name of the design document
+        :param view: name of the view
+        :return: iterator returning Token objects
+        """
         return TokenViewIterator(self, design_doc, view)
     
     def save(self, doc):
+        """ Save a Document to the database.
+        
+        Updates the document to have the new _rev value.
+        :param doc: Document object
+        """
         _, _rev = self.db.save(doc.value)
         doc['_rev'] = _rev
         return doc
@@ -89,10 +120,25 @@ class CouchDB(object):
         return result
 
     def add_view(self, design_doc, view, map_fun, reduce_fun=None, *args, **kwargs):
+        """ Add a view to the database
+        All extra parameters are passed to couchdb.design.ViewDefinition
+        :param design_doc: name of the design document
+        :param view: name of the view
+        :param map_fun: string of the javascript map function
+        :param reduce_fun: string of the javascript reduce function (optional)
+        """
         definition = ViewDefinition('Monitor', view, map_fun, reduce_fun, *args, **kwargs)
         definition.sync(self.db)
 
     def delete_tokens(self, tokens):
+        """
+        Delete a sequence of tokens from the database.
+        
+        The tokens must have a valid and current _id and _rev, so they must be
+        retrieved from the database and not be altered there in the mean time.
+        :param tokens: list of Token objects
+        :return: array of booleans indicating whether the respective token was deleted.
+        """
         result = np.ones(len(tokens),dtype=np.bool)
         for i, token in enumerate(tokens):
             try:
@@ -107,6 +153,13 @@ class CouchDB(object):
         return result
 
     def delete_tokens_from_view(self, design_doc, view):
+        """
+        Delete all tokens in a view
+        
+        :param design_doc: name of the design document
+        :param view: name of the view
+        :return: array of booleans indicating whether the respective tokens were deleted
+        """
         tokens = self.get_all_tokens(design_doc, view)
         return self.delete_tokens(tokens)
 
@@ -142,7 +195,7 @@ class ViewIterator(object):
     def claim_token(self, allowed_failures=10):
         """
         Get the first available token from a view.
-        @param allowed_failures: the number of times a lock failure may
+        :param allowed_failures: the number of times a lock failure may
         occur before giving up. Default=10.
         """
         raise NotImplementedError("claim_token function not implemented.")
