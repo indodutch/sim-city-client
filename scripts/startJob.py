fa57edd4c239a8d878128e69f2900e4a8cea9e16
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-02-05 09:58:24 +0100

    Start and submit jobs in separate scripts

--- scripts/startJob.py
+++ scripts/startJob.py
@@ -13,22 +13,16 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="start a job")
-    parser.add_argument('command', help="command to run")
     parser.add_argument('host', help="host to run pilot job on")
+    parser.add_argument('-m', '--max', help="only run if there are less than MAX jobs running", default=2)
     args = parser.parse_args() 
 
     simcity = simcity_client.init()
-    try:
-        token = simcity_client.add_token({'command': args.command}, simcity['database'])
-        print "token", token.id, "added to the database"
-    except Exception as ex:
-        print "Token could not be added to the database:", ex
-        sys.exit(1)
 
     num = simcity_client.overview_total(simcity['database'])
     print "Overview", num
     num_jobs = num['active_jobs'] + num['pending_jobs']
-    if num_jobs <= num['todo'] and num_jobs < 2:
+    if num_jobs <= num['todo'] and num_jobs < args.max:
         print "Starting new job"
         # try:
         job = simcity_client.start_job(args.host, simcity['database'], simcity['config'])
