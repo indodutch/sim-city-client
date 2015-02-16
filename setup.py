abd590e7ed2ae38d086ba0d4bce1127db712d7b0
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-02-16 14:08:33 +0100

    Using separate requirements files

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-test_requirements = ['nose', 'coverage']
 setup(name='simcity',
       version='0.2',
       description='Python SIM-CITY client using CouchDB as a task pool server.',
@@ -9,6 +8,4 @@ setup(name='simcity',
       author_email='j.borgdorff@esciencecenter.nl',
       url='https://esciencecenter.nl/projects/sim-city/',
       packages=['simcity', 'simcity.task', 'simcity.job'],
-      install_requires=['couchdb', 'pystache', 'numpy'] + test_requirements,
-      tests_require=test_requirements,
      )
