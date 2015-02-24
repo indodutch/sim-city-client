ba441709490e685a3b7b5b5e45332819f05cb7d9
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-02-24 13:41:41 +0100

    Added topic classifiers

--- setup.py
+++ setup.py
@@ -2,10 +2,24 @@
 
 from distutils.core import setup
 setup(name='simcity',
-      version='0.2',
+      version='0.2.1',
       description='Python SIM-CITY client using CouchDB as a task pool server.',
       author='Joris Borgdorff',
       author_email='j.borgdorff@esciencecenter.nl',
       url='https://esciencecenter.nl/projects/sim-city/',
       packages=['simcity', 'simcity.task', 'simcity.job'],
+      classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: System :: Distributed Computing'
+      ],
      )
