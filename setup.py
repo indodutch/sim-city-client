541a61ef87c193e58b177a6012ba4c3fd6de8ffa
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-11 17:29:40 +0100

    Updated version information

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 
 from distutils.core import setup
 setup(name='simcity',
-      version='0.2.1',
+      version='0.2.2',
       description='Python SIM-CITY client using CouchDB as a task pool server.',
       author='Joris Borgdorff',
       author_email='j.borgdorff@esciencecenter.nl',
